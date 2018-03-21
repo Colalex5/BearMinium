e81bbc5f7f47d16335a07d696b330dbb70e4164d
Alex Thelin <36939391+Colalex5@users.noreply.github.com>
2018-03-21 13:56:54 -0600

    Did entire login

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,32 @@
-package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        boolean lol = true;
+        Scanner myScanner=new Scanner(System.in);
+        String usernam, passwor = null, username = null, password;
+        do {
+
+            System.out.println("Sign in or sign up");
+            String ino = myScanner.nextLine().toLowerCase();
+             lol = true;
+            if (ino.matches("sign in")) {
+                    System.out.println("What is your username: ");
+                    usernam = myScanner.nextLine();
+                    System.out.println("What is your password: ");
+                    password = myScanner.nextLine();
+                    if(usernam == username && passwor == password)
+                lol=false;
+            } else {
+                System.out.println("What would you like your username to be? ");
+                usernam = myScanner.nextLine();
+                System.out.println("What would you like your password to be? ");
+                password = myScanner.nextLine();
+                lol = true;
+            }
+        }while(lol!=false);
+
+
     }
 }
